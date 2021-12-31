735408d3ee1dc40df4e04f1e9e1c6b8ef2e59524
Diksha1720 <dikshabharti39@gmail.com>
2021-12-31 13:05:47 +0530

    updated the questions

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+
 question_data = [
 {"text": "A slug's blood is green.", "answer": "True"},
 {"text": "The loudest animal is the African Elephant.", "answer": "False"},
@@ -12,3 +13,4 @@ question_data = [
 {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
 {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
 ]
+
